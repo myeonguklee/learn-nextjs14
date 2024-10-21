ff9044eb14c1883ce5e8996d7494ee374ba02911
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-10-22 06:54:25 +0900

    feat: Metadata Template

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,25 @@
-import Navigation from "../components/navigation"
+import { Metadata } from "next";
+import Navigation from "../components/navigation";
 
-export const metadata = {
-  description: 'The best movies on the best frameworks',
-} 
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Next Movies",
+    default: "Next Movies",
+  },
+  description: "The best movies on the best frameworks",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
         <Navigation />
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
