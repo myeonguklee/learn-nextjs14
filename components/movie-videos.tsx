a115937c21cebe3756bfe240715ccdf40a2a7ac4
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-12-02 23:52:45 +0900

    feat: error handling

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,10 +1,9 @@
-import {API_URL} from "../consts/url";
-
 async function getVideos(id: string) {
   console.log(`Fetching videos: ${Date.now()}`)
   await new Promise(resolve => setTimeout(resolve, 3000))
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
+  throw new Error("something broke...")
+  // const response = await fetch(`${API_URL}/${id}/videos`);
+  // return response.json();
 }
 
 export default async function MovieVideos({id}: { id: string }) {
