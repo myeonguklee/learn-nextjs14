c95574f4ec534dc2cffe1fd216128091ae1666cd
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-12-02 21:33:56 +0900

    feat: client side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,23 @@
-export const metadata = {
-  title: 'Home',
-}
+'use client'
 
-export default function Tomata() {
+import {useEffect, useState} from "react";
+
+export default function Page() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch('https://nomad-movies.nomadcoders.workers.dev/movies');
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, [])
   return (
     <div>
-      <h1>Hello NextJs!</h1>
+      {isLoading ? "Loading..." : JSON.stringify(movies)}
     </div>
   );
 }
+
